67f506247bf6526429927e7a175fd139dceb5451
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-23 20:49:28 -0300

    Criação da entidade dentro da model

---
+++ src/main/java/com/nika/ex1_lista_javaspringboot/models/Livro.java
@@ -0,0 +1,33 @@
+package com.nika.ex1_lista_javaspringboot.models;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Livro")
+@Table(name = "tb_livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo_livro")
+    private String titulo;
+
+    @Column(name = "nome_autor")
+    private String autor;
+
+    private Integer anoPublicacao;
+
+    // getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+
+    public Integer getAnoPublicacao() { return anoPublicacao; }
+    public void setAnoPublicacao(Integer anoPublicacao) { this.anoPublicacao = anoPublicacao; }
+}
